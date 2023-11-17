daab68a58c2697935bb992b2b835edf4795016a4
AetherFabian <fabian_galarza@hotmail.com>
2023-11-17 11:33:32 -0600

    fix: reference error post is named pos

--- src/api/routes/esp32-battery.routes.js
+++ src/api/routes/esp32-battery.routes.js
@@ -4,7 +4,7 @@ const router = Router();
 const esp32BatteryController = require('../controllers/esp32-battery.controller');
 
 router.get('/', esp32BatteryController.get);
-router.post('/', esp32BatteryController.pos);
+router.post('/', esp32BatteryController.post);
 router.patch('/', esp32BatteryController.patch);
 
 module.exports = router;
